0a2191fd65fb4fd649162f964a4f11df57201694
IvanBaikal <verbitskyia@palitra-system.ru>
2022-05-05 10:28:23 +0300

    Task #3 is Done

--- task_1/solution.js
+++ task_1/solution.js
@@ -25,11 +25,6 @@ function addInCartCountChange(value) {
     newValue = oldValue;
     newValue = newValue + 1;
 
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
-    // Конец решения задания №1.2.
 
     return newValue;
 }
@@ -44,6 +39,10 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
+    let = newSum = oldSum + difference;
+    let = newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`
+    
     // Конец решения задания №1.3.
 
     return newSumText;
