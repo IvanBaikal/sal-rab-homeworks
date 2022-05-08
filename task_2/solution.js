5be7273a3a2d8ea13c81626585d2c95620a94665
IvanBaikal <verbitskyia@palitra-system.ru>
2022-05-08 15:51:19 +0300

    2.1 is Done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,6 +6,7 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum;
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -13,13 +14,20 @@ function calcShipping(sum, min, shipping) {
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     
+    if(productsSum == 0 || productsSum >= freeShippingMinSum){ 
+        shippingSum == 0; 
+    }  
+    
+    if(productsSum >0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice
+    }
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
 
-    return shippingSum;
+    return shippingPrice;
 }
 
 function calcDiscount(sum, min, discount) {
@@ -30,9 +38,17 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
+    let discountSum;
 
     // если productsSum больше или равно discountMinSum,
+    if(productsSum >= discountMinSum);{
+        discountSum == discountPart / 100 * productsSum
+
+    }else{discountSum == 0
+
+    }
     // то присвойте discountSum значение discountPart процентов от productsSum,
+
     // иначе присвойте discountSum значение 0
 
     // Конец решения задания №2.2.
