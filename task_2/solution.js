a21d151bfd424fc145e3ee390d35de14b4416fc1
IvanBaikal <verbitskyia@palitra-system.ru>
2022-05-08 21:48:14 +0300

    2.1 and 2.2 is Done

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,10 +4,8 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
-
-    let shippingSum;
+    
+    let shippingSum; // создайте переменную shippingSum
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -16,21 +14,15 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение 0
     
     if(productsSum == 0 || productsSum >= freeShippingMinSum){ 
-        shippingSum == 0;
-        }
-   
-    if(productsSum > 0 && productsSum < freeShippingMinSum){
-        shippingSum = shippingPrice;
+        shippingSum = 0;
 
-        }  
-    
+    }else if(productsSum > 0 && productsSum < freeShippingMinSum){
+            shippingSum = shippingPrice;
+    }
+   
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-  
-    
-
-
-    // Конец решения задания №2.1.
+        // Конец решения задания №2.1.
 
     return shippingPrice;
 }
@@ -87,7 +79,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let freeShipping;
 
     (shippingSum == 0) ? freeShipping = true : freeShipping = false;
-    
+
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
